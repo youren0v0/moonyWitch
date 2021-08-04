f35d9fc5478a4ead0a8367e1de6d131f5d8bef4b
youren0v0 <youren0v0@sina.com>
2021-08-04 16:56:28 +0800

    修复action中router路径错误

--- config/deploy.js
+++ config/deploy.js
@@ -1,5 +1,5 @@
 // 给每个route都新建一个文件夹，把index.html拷贝进去
-const routers = require("../src/utils/routers");
+const routers = require("../static/json/router.json");
 const fs = require("fs-extra");//添加本机文件的工具
 const path = require("path"); //处理文件路径的工具
 routers.forEach((v) => {
